d3f3f8820e3c3d215dd289d1a23301c3301badb4
jianpo.hjp <jianpo.hjp@alibaba-inc.com>
2020-04-21 20:12:36 +0800

    对齐注释

--- src/main/java/com/lc/algorithms/lc24/SwapNodesInPairs.java
+++ src/main/java/com/lc/algorithms/lc24/SwapNodesInPairs.java
@@ -50,9 +50,9 @@ public class SwapNodesInPairs {
     }
 
     /**
-     * node1 node2 next
+     *         node1 node2 next
      * xHead -> 1  -> 2  -> 3  -> 4  -> 5
-     * node2 node1 next
+     *         node2 node1 next
      * xHead -> 2  -> 1  -> 3  -> 4  -> 5
      */
     public static ListNode swapPairs(ListNode head) {
