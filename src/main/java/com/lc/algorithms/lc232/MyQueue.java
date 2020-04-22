5a977c4e7378e709ad79026f5a423ccad219201e
jianpo.hjp <jianpo.hjp@alibaba-inc.com>
2020-04-22 13:53:05 +0800

    增加注释

--- src/main/java/com/lc/algorithms/lc232/MyQueue.java
+++ src/main/java/com/lc/algorithms/lc232/MyQueue.java
@@ -10,38 +10,43 @@ import java.util.Stack;
 public class MyQueue {
 
     /**
-     * 入队(栈)
+     * 声明入队(栈)
      */
-    private Stack<Integer> input = new Stack<>();
+    private Stack<Integer> input;
 
     /**
-     * 出队(栈)
+     * 声明出队(栈)
      */
-    private Stack<Integer> output = new Stack<>();
+    private Stack<Integer> output;
 
     /**
      * Initialize your data structure here.
+     * 初始化两个堆栈
      */
     public MyQueue() {
-
+        input = new Stack<>();
+        output = new Stack<>();
     }
 
     /**
      * Push element x to the back of queue.
+     * 入队(压入input栈)
      */
     public void push(int x) {
-        // 压入入队(栈)
+        // 入队(压入input栈)
         input.push(x);
     }
 
     /**
      * Removes the element from in front of queue and returns that element.
+     * 出队，若是output为空，则将input中数据导入到output中，然后在从output中出栈(队)
      */
     public int pop() {
         if (output.isEmpty()) {
-            // 若是出队堆栈为空，则将入队堆栈中的数据导入到出栈队列中
+            // 若是output为空，则将input中数据导入到output中
             changeInput2Ouuput();
         }
+        // 从output中出栈(队)
         return output.pop();
     }
 
@@ -59,19 +64,23 @@ public class MyQueue {
 
     /**
      * Get the front element.
+     * 查看队头元素，若是output为空，则将input中数据导入到output中，然后在从output中查看栈顶(队头)元素
      */
     public int peek() {
         if (output.isEmpty()) {
-            // 若是出队堆栈为空，则将入队堆栈中的数据导入到出栈队列中
+            // 若是output为空，则将input中数据导入到output中
             changeInput2Ouuput();
         }
+        // 从output中查看栈顶(队头)元素
         return output.peek();
     }
 
     /**
      * Returns whether the queue is empty.
+     * 判断队列是否为空，只有input和output同时为空，才为空
      */
     public boolean empty() {
+        // 判断队列是否为空，只有input和output同时为空，才为空
         return output.isEmpty() && input.isEmpty();
     }
 }
